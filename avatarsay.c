8c0075f329988311f14ee6f68e288dfa352c7362
Andreas K. Foerster <info@akfoerster.de>
2008-01-20 13:47:24 +0000

    trying to support function keys - it's a mess

--- avatarsay.c
+++ avatarsay.c
@@ -18,7 +18,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-/* $Id: avatarsay.c,v 2.65 2008-01-20 09:41:04 akf Exp $ */
+/* $Id: avatarsay.c,v 2.66 2008-01-20 13:47:24 akf Exp $ */
 
 #ifndef _GNU_SOURCE
 #  define _GNU_SOURCE
@@ -1283,25 +1283,95 @@ prg_keyhandler (int sym, int mod, int unicode)
 
 	case 277:		/* Insert */
 	  write (prg_input, "\033[L", 3);
-	  /* write (prg_input, "\033[2~", 4); */ /* linux */
+	  /* write (prg_input, "\033[2~", 4); *//* linux */
 	  break;
 
 	case 278:		/* Home */
 	  write (prg_input, "\033[H", 3);
-	  /* write (prg_input, "\033[1~", 4); */ /* linux */
+	  /* write (prg_input, "\033[1~", 4); *//* linux */
 	  break;
 
 	case 279:		/* End */
 	  write (prg_input, "\033[0w", 4);
-	  /* write (prg_input, "\033[4~", 4); */ /* linux */
+	  /* write (prg_input, "\033[4~", 4); *//* linux */
 	  break;
 
 	case 280:		/* Page up */
-	  write (prg_input, "\033[5~", 4); /* linux */
+	  write (prg_input, "\033[5~", 4);	/* linux */
 	  break;
 
 	case 281:		/* Page down */
-	  write (prg_input, "\033[6~", 4); /* linux */
+	  write (prg_input, "\033[6~", 4);	/* linux */
+	  break;
+
+	case 282:		/* F1 */
+	  write (prg_input, "\033[[A", 4);	/* linux */
+	  /* write (prg_input, "\033OP", 3); *//* DEC */
+	  break;
+
+	case 283:		/* F2 */
+	  write (prg_input, "\033[[B", 4);	/* linux */
+	  /* write (prg_input, "\033OQ", 3); *//* DEC */
+	  break;
+
+	case 284:		/* F3 */
+	  write (prg_input, "\033[[C", 4);	/* linux */
+	  /* write (prg_input, "\033OR", 3); *//* DEC */
+	  break;
+
+	case 285:		/* F4 */
+	  write (prg_input, "\033[[D", 4);	/* linux */
+	  /* write (prg_input, "\033OS", 3); *//* DEC */
+	  break;
+
+	case 286:		/* F5 */
+	  write (prg_input, "\033[[E", 4);	/* linux */
+	  /* write (prg_input, "\033Ot", 3); *//* DEC */
+	  break;
+
+	case 287:		/* F6 */
+	  write (prg_input, "\033[17~", 5);	/* linux */
+	  /* write (prg_input, "\033Ou", 3); *//* DEC */
+	  break;
+
+	case 288:		/* F7 */
+	  write (prg_input, "\033[[18~", 5);	/* linux */
+	  /* write (prg_input, "\033Ov", 3); *//* DEC */
+	  break;
+
+	case 289:		/* F8 */
+	  write (prg_input, "\033[19~", 5);	/* linux */
+	  /* write (prg_input, "\033Ol", 3); *//* DEC */
+	  break;
+
+	case 290:		/* F9 */
+	  write (prg_input, "\033[20~", 5);	/* linux */
+	  /* write (prg_input, "\033Ow", 3); *//* DEC */
+	  break;
+
+	case 291:		/* F10 */
+	  write (prg_input, "\033[21~", 5);	/* linux */
+	  /* write (prg_input, "\033Ox", 3); *//* DEC */
+	  break;
+
+	case 292:		/* F11 */
+	  write (prg_input, "\033[23~", 5);	/* linux */
+	  break;
+
+	case 293:		/* F12 */
+	  write (prg_input, "\033[24~", 5);	/* linux */
+	  break;
+
+	case 294:		/* F13 */
+	  write (prg_input, "\033[25~", 5);	/* linux */
+	  break;
+
+	case 295:		/* F14 */
+	  write (prg_input, "\033[26~", 5);	/* linux */
+	  break;
+
+	case 296:		/* F15 */
+	  write (prg_input, "\033[27~", 5);	/* linux */
 	  break;
 
 	default:
@@ -2615,7 +2685,7 @@ main (int argc, char *argv[])
   quit (EXIT_SUCCESS);
 
   /* never executed, but kept in the code */
-  puts ("$Id: avatarsay.c,v 2.65 2008-01-20 09:41:04 akf Exp $");
+  puts ("$Id: avatarsay.c,v 2.66 2008-01-20 13:47:24 akf Exp $");
 
   return EXIT_SUCCESS;
 }
