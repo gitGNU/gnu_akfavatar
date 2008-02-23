8f5d277745da39fae230e21453ecd6d16ab0d930
Andreas K. Foerster <info@akfoerster.de>
2008-02-23 08:58:19 +0000

    selfcontained batch files

--- mingw32/fsdemo-de.bat
+++ mingw32/fsdemo-de.bat
@@ -1,3 +1,47 @@
 @echo off
+avatarsay %0
+exit
 
-avatarsay --utf-8 fsdemo-de
+.encoding UTF-8
+.backgroundcolor #BBBB88
+
+#.datadir /home/akf/Bilder
+# datadir - wo sich die Medien-Dateien befinden
+# Kann auch über die Umgebungsvariable AVATARDATADIR gesetzt werden
+
+#.avatarimage human-peasant.bmp
+# avatarimage - welches Bild als Avatar verwendet wwerden soll 
+# muss vor dem auszugebenden Text angegeben werden!
+# Kann auch über die Umgebungsvariable AVATARIMAGE gesetzt werden
+# (mit vollständiger Pfadangabe)
+
+Bei dem Begriff „Freie Software“ geht es um Freiheit, nicht um den Preis.
+Um dieses Konzept richtig begreifen zu können, sollte man an „frei“ wie in
+„freie Rede“ denken...\
+.effectpause
+.back 3
+, und nicht an „Freibier“.
+
+Bei „Freier Software“ geht es um die Freiheit des Benutzers die Software nach
+Belieben zu benutzen, zu kopieren, weiter zu geben, die Software zu studieren,
+sowie Änderungen und Verbesserungen an der Software vornehmen zu können.
+------------------------------------------------------------------------------
+Genauer gesagt, bezieht sich der Begriff „Freie Software“ auf vier Arten von
+Freiheit, die der Benutzer der Software hat:
+
+0. Die Freiheit, das Programm für jeden Zweck zu benutzen.
+1. Die Freiheit, zu verstehen, wie das Programm funktioniert und wie man es
+   für seine Ansprüche anpassen kann.
+   Der Zugang zum Quellcode ist dafür Voraussetzung.
+2. Die Freiheit, Kopien weiterzuverbreiten, so dass man seinem Nächsten
+   weiterhelfen kann.
+3. Die Freiheit, das Programm zu verbessern und die Verbesserungen der
+   Allgemeinheit zur Verfügung zu stellen, damit die ganze Gemeinschaft davon
+   profitieren kann. Der Zugang zum Quellcode ist dafür Voraussetzung.
+------------------------------------------------------------------------------
+Diese 4 Freiheiten (0-3) wurden so von Richard Stallman entworfen.
+
+Um mit gutem Beispiel voran zu gehen, hat er angefangen, ein vollständig
+freies Betriebssystem zu entwickeln.
+
+Heute werden Linux basierte GNU Systeme von vielen Millionen Anwendern benutzt.
