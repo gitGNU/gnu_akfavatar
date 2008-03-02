21c431954f32faa8e3d2ed0e8311a269a901965f
Andreas K. Foerster <info@akfoerster.de>
2008-03-02 14:13:53 +0000

    wait for key after executing a program

--- avatarsay.c
+++ avatarsay.c
@@ -18,7 +18,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-/* $Id: avatarsay.c,v 2.114 2008-03-02 11:22:58 akf Exp $ */
+/* $Id: avatarsay.c,v 2.115 2008-03-02 14:13:53 akf Exp $ */
 
 #ifndef _GNU_SOURCE
 #  define _GNU_SOURCE
@@ -2937,7 +2937,7 @@ main (int argc, char *argv[])
       if (fd > -1)
 	process_subprogram (fd);
 
-      if (avt_flip_page ())
+      if (avt_wait_button ())
 	quit (EXIT_SUCCESS);
 
       if (initialized && !popup)
@@ -2966,7 +2966,7 @@ main (int argc, char *argv[])
   quit (EXIT_SUCCESS);
 
   /* never executed, but kept in the code */
-  puts ("$Id: avatarsay.c,v 2.114 2008-03-02 11:22:58 akf Exp $");
+  puts ("$Id: avatarsay.c,v 2.115 2008-03-02 14:13:53 akf Exp $");
 
   return EXIT_SUCCESS;
 }
