f7edf730f9189abec18f79a6044bd4ed16a2a25d
Andreas K. Foerster <info@akfoerster.de>
2008-06-20 20:37:22 +0000

    fix: also prevent man from becomming a zombie

--- avatarsay.c
+++ avatarsay.c
@@ -18,7 +18,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-/* $Id: avatarsay.c,v 2.153 2008-06-20 19:54:42 akf Exp $ */
+/* $Id: avatarsay.c,v 2.154 2008-06-20 20:37:22 akf Exp $ */
 
 #ifndef _GNU_SOURCE
 #  define _GNU_SOURCE
@@ -2569,7 +2569,7 @@ process_subprogram (int fd)
   avt_bool_t stop;
   wint_t ch;
   wchar_t last_character;
-  int status;
+  int exitcode;
   const wchar_t vt100trans[] = {
     0x00A0, 0x25C6, 0x2592, 0x2409, 0x240C, 0x240D,
     0x240A, 0x00B0, 0x00B1, 0x2424, 0x240B,
@@ -2625,9 +2625,9 @@ process_subprogram (int fd)
   if (close (fd) == -1 && errno != EAGAIN)
     warning_msg ("close", strerror (errno));
 
-  /* fetch process status, just to prevent zombies */
-  wait (&status);
-  
+  /* fetch process exitcode, just to prevent zombies */
+  wait (&exitcode);
+
   /* release keyhandler */
   avt_register_keyhandler (NULL);
   prg_input = -1;
@@ -2856,6 +2856,7 @@ ask_manpage (void)
 {
   char manpage[AVT_LINELENGTH] = "man";
   char *argv[] = { "man", "-t", (char *) &manpage, NULL };
+  int exitcode;
 
   avt_clear ();
   avt_set_text_delay (0);
@@ -2887,6 +2888,9 @@ ask_manpage (void)
 
       if (fd > -1)
 	process_file (fd);
+
+      /* fetch process exitcode, just to prevent zombies */
+      wait (&exitcode);
 
       status = avt_get_status ();
       if (status == AVT_ERROR)
@@ -3395,7 +3399,7 @@ main (int argc, char *argv[])
   quit (EXIT_SUCCESS);
 
   /* never executed, but kept in the code */
-  puts ("$Id: avatarsay.c,v 2.153 2008-06-20 19:54:42 akf Exp $");
+  puts ("$Id: avatarsay.c,v 2.154 2008-06-20 20:37:22 akf Exp $");
 
   return EXIT_SUCCESS;
 }
