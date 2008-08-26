b582a2c54a3d9ba3ac5b3b0ea66e60014413ce47
Andreas K. Foerster <info@akfoerster.de>
2008-08-26 19:11:28 +0000

    comment

--- avatarsay.c
+++ avatarsay.c
@@ -18,7 +18,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-/* $Id: avatarsay.c,v 2.173 2008-08-26 19:08:51 akf Exp $ */
+/* $Id: avatarsay.c,v 2.174 2008-08-26 19:11:28 akf Exp $ */
 
 #ifndef _GNU_SOURCE
 #  define _GNU_SOURCE
@@ -766,6 +766,7 @@ use_avatar_image (char *image_file)
     free (avt_image_name);
 
   /* save the name */
+  /* FIXME: memory leak, when called from menu */
   avt_image_name = strdup (image_file);
 
   avt_image = avt_import_image_file (image_file);
@@ -3832,7 +3833,7 @@ main (int argc, char *argv[])
   quit (EXIT_SUCCESS);
 
   /* never executed, but kept in the code */
-  puts ("$Id: avatarsay.c,v 2.173 2008-08-26 19:08:51 akf Exp $");
+  puts ("$Id: avatarsay.c,v 2.174 2008-08-26 19:11:28 akf Exp $");
 
   return EXIT_SUCCESS;
 }
