fc7862a2c58b0666bd9a3c7d37ed5770f56c7f7e
Andreas K. Foerster <info@akfoerster.de>
2008-09-12 13:58:38 +0000

    MinGW: check if we need to add the drive letter

--- avatarsay.c
+++ avatarsay.c
@@ -18,7 +18,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-/* $Id: avatarsay.c,v 2.195 2008-09-12 13:05:15 akf Exp $ */
+/* $Id: avatarsay.c,v 2.196 2008-09-12 13:58:38 akf Exp $ */
 
 #ifndef _GNU_SOURCE
 #  define _GNU_SOURCE
@@ -343,7 +343,9 @@ get_user_home (void)
   homepath = getenv ("HOMEPATH");
   homedrive = getenv ("HOMEDRIVE");
 
-  if (homedrive && homepath)
+  /* do we need to add the drive-letter? */
+  if (homedrive && homepath && *(homepath + 1) != ':'
+      && *(homepath + 1) != '\\')
     {
       strcpy (buf, homedrive);
       strcat (buf, homepath);
@@ -2630,7 +2632,7 @@ main (int argc, char *argv[])
   exit (EXIT_SUCCESS);
 
   /* never executed, but kept in the code */
-  puts ("$Id: avatarsay.c,v 2.195 2008-09-12 13:05:15 akf Exp $");
+  puts ("$Id: avatarsay.c,v 2.196 2008-09-12 13:58:38 akf Exp $");
 
   return EXIT_SUCCESS;
 }
