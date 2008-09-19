f89a424ef474a916cb2c29243238eb9d0c26bb78
Andreas K. Foerster <info@akfoerster.de>
2008-09-19 12:42:29 +0000

    avoid GNU extension for portability

--- avatarsay.c
+++ avatarsay.c
@@ -18,7 +18,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-/* $Id: avatarsay.c,v 2.205 2008-09-19 12:28:20 akf Exp $ */
+/* $Id: avatarsay.c,v 2.206 2008-09-19 12:42:29 akf Exp $ */
 
 #ifndef _GNU_SOURCE
 #  define _GNU_SOURCE
@@ -731,10 +731,10 @@ first_archive_member (int fd, char *member)
 
       /* find end of the name */
       /* either terminated by / or by space */
-      end = (char *) memrchr (member, '/', sizeof (header.name));
+      end = (char *) memchr (member, '/', sizeof (header.name));
 
       if (!end)
-	end = (char *) memrchr (member, ' ', sizeof (header.name));
+	end = (char *) memchr (member, ' ', sizeof (header.name));
 
       if (end)
 	*end = '\0';		/* make it a valid C-string */
@@ -2643,7 +2643,7 @@ main (int argc, char *argv[])
   exit (EXIT_SUCCESS);
 
   /* never executed, but kept in the code */
-  puts ("$Id: avatarsay.c,v 2.205 2008-09-19 12:28:20 akf Exp $");
+  puts ("$Id: avatarsay.c,v 2.206 2008-09-19 12:42:29 akf Exp $");
 
   return EXIT_SUCCESS;
 }
