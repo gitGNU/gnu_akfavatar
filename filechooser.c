7ae34cde93c381fe20cd535f24a9bbe27986d95b
Andreas K. Foerster <info@akfoerster.de>
2009-08-04 10:30:35 +0200

    filechooser.c: renamed from avtfilechooser

--- filechooser.c
+++ filechooser.c
@@ -1,5 +1,5 @@
 /*
- * avtfilechooser - filechooser dialog for AKFAvatar
+ * filechooser - filechooser dialog for AKFAvatar
  * Copyright (c) 2008, 2009 Andreas K. Foerster <info@akfoerster.de>
  *
  * This program is free software; you can redistribute it and/or modify
