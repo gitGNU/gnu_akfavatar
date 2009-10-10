3475d87b2392aa25f0f269012ea52c82bebb3659
Andreas K. Foerster <info@akfoerster.de>
2009-10-10 12:03:24 +0200

    colorchooser: allow manual entry

--- colorchooser.c
+++ colorchooser.c
@@ -2,12 +2,14 @@
  * colorchooser - colorchooser dialog for AKFAvatar
  * Copyright (c) 2009 Andreas K. Foerster <info@akfoerster.de>
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of AKFAvatar
+ *
+ * AKFAvatar is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * AKFAvatar is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -21,6 +23,9 @@
 /* House symbol */
 #define HOME L" \x2302 "
 
+/* Keyboard symbol */
+#define KEYBOARD L" \x2328 "
+
 /* three arrows up */
 #define BACK L" \x2191 \x2191 \x2191 "
 
@@ -32,12 +37,32 @@
          do { avt_set_text_background_color (0xdd, 0xdd, 0xdd); \
          avt_say (S); avt_normal_text (); } while(0)
 
+static char *
+manual_entry (void)
+{
+  static char manual_color[80];	/* must be static! */
+  int red, green, blue;
+
+  avt_set_balloon_height (1);
+  avt_say (L"> ");
+  avt_lock_updates (AVT_FALSE);
+
+  if (avt_ask_mb (manual_color, sizeof (manual_color)) != AVT_NORMAL)
+    return NULL;
+
+  /* check, if it's a valid color name */
+  if (avt_name_to_color (manual_color, &red, &green, &blue) > -1)
+    return manual_color;
+  else
+    return NULL;
+}
+
 extern const char *
 avta_color_selection (void)
 {
   const char *color, *c;
   int i;
-  int max_idx, items, page_entries, page_nr;
+  int max_idx, items, offset, page_nr;
   int choice;
 
   avt_set_text_delay (0);
@@ -49,7 +74,6 @@ avta_color_selection (void)
 
   color = c = NULL;
   max_idx = avt_get_max_y ();
-  page_entries = max_idx - 2;	/* minus back and forward entries */
 
   page_nr = 0;
 
@@ -61,11 +85,19 @@ avta_color_selection (void)
 
       MARK ((page_nr > 0) ? BACK : HOME);
       avt_new_line ();
-      items = 1;
+      items = offset = 1;
 
-      for (i = 0; i < page_entries; i++)
+      if (page_nr == 0)
 	{
-	  c = avt_get_color_name (i + (page_nr * page_entries));
+	  MARK (KEYBOARD);
+	  avt_new_line ();
+	  items++;
+	  offset++;
+	}
+
+      for (i = 0; i < max_idx - offset - 1; i++)
+	{
+	  c = avt_get_color_name (i + (page_nr * (max_idx - offset)));
 	  if (c)
 	    {
 	      avt_set_text_background_color_name (c);
@@ -79,7 +111,7 @@ avta_color_selection (void)
 	}
 
       if (c != NULL)
-        {
+	{
 	  MARK (CONTINUE);
 	  items = max_idx;
 	}
@@ -96,8 +128,14 @@ avta_color_selection (void)
 	break;			/* home */
       else if (choice == max_idx)
 	page_nr += (c == NULL) ? 0 : 1;	/* page forward */
+      else if (page_nr == 0 && choice == 2)
+	{
+	  color = (const char *) manual_entry ();
+	  break;
+	}
       else
-	color = avt_get_color_name (choice - 2 + (page_nr * page_entries));
+	color = avt_get_color_name (choice - 1 - offset
+				    + (page_nr * (max_idx - offset)));
     }
 
   avt_auto_margin (AVT_TRUE);
