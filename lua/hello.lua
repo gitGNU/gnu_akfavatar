21b33a9f6015f1a3b06ce5664f2b6272310859c9
Andreas K. Foerster <info@akfoerster.de>
2010-06-26 12:25:41 +0200

    shorter license boilerplate for short Lua scripts

--- lua/hello.lua
+++ lua/hello.lua
@@ -1,22 +1,7 @@
 #!/usr/bin/env lua-akfavatar
 
---[[
-hello world for AKFAvatar
-Copyright (c) 2009,2010 Andreas K. Foerster <info@akfoerster.de>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
-]]
+-- Copyright (c) 2009,2010 Andreas K. Foerster <info@akfoerster.de>
+-- License: GPL version 3 or later
 
 -- Note: Since this is a trivial example, you don't have to keep my
 -- Copyright notice, but you may replace it with your own.
