c9bf4781fc6ef8419b6f05a12a5620f7537917c5
Andreas K. Foerster <info@akfoerster.de>
2010-07-22 17:33:16 +0200

    lua/interactive_lua.lua: further optimizations

--- lua/interactive_lua.lua
+++ lua/interactive_lua.lua
@@ -5,19 +5,16 @@
 
 require "lua-akfavatar"
 
-local prompt1 = "> "
-local prompt2 = ">> "
-
 print = avt.print --> define the print command
 say = print --> an alias that fits better
 
-local function say_line (s) avt.say (s); avt.newline () end
+local function process (cmd)
+  local function say_line (s) avt.say (s); avt.newline () end
 
-local function process (prompt, cmd)
-  avt.say (prompt)
+  if not cmd then avt.say ("> ") else avt.say (">> ") end
   local line = avt.ask ()
 
-  if cmd == "" then --> first line
+  if not cmd then --> first line
     -- replace = at the beginning with return
     cmd = string.gsub (line, "^=", "return ", 1)
   else
@@ -30,7 +27,7 @@ local function process (prompt, cmd)
     then
       -- '<eof>' at the end means the command is incomplete
       if (string.find(err, "'<eof>'$"))
-        then return process (prompt2, cmd)
+        then return process (cmd)
         else say_line (err) --> say error mesage
       end
     else --> complete function
@@ -41,11 +38,12 @@ local function process (prompt, cmd)
         else if ret[1] ~= nil then say_line (unpack (ret)) end
       end
   end
+
+  return cmd
 end
 
 avt.initialize {title="Lua-AKFAvatar", encoding="UTF-8", audio=true}
 avt.move_in ()
 avt.say(_VERSION, " / AKFAvatar ", avt.version (), "\n\n")
 
-while true do process (prompt1, "") end
-
+while true do process () end
