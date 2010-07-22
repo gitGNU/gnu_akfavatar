26c34019ac4d8be47c03ead14ca4f4f58269370e
Andreas K. Foerster <info@akfoerster.de>
2010-07-22 15:06:31 +0200

    lua/interactive_lua.lua: incomplete lines can be continued

--- lua/interactive_lua.lua
+++ lua/interactive_lua.lua
@@ -5,29 +5,47 @@
 
 require "lua-akfavatar"
 
-local prompt = "> "
-local cmd, func, err, success
+local prompt1 = "> "
+local prompt2 = ">> "
 
 print = avt.print --> define the print command
 say = print --> an alias that fits better
 
-avt.initialize{title="Lua-AKFAvatar", encoding="UTF-8", audio=true}
-avt.move_in()
-avt.say(_VERSION, " / AKFAvatar ", avt.version(), "\n\n")
+local function say_line (s) avt.say (s); avt.newline () end
 
-repeat
-  avt.say(prompt)
-  cmd = avt.ask()
-  cmd = string.gsub(cmd, "^=", "return ", 1)
-  func, err = loadstring(cmd)
-  if not func
-    then print(err)
-    else
+local function process (prompt, cmd)
+  avt.say (prompt)
+  local line = avt.ask ()
+
+  if cmd == "" then --> first line
+    -- replace = at the beginning with return
+    cmd = string.gsub (line, "^=", "return ", 1)
+  else
+    cmd = cmd .. " " .. line
+  end
+
+  local func, err = loadstring (cmd)
+
+  if func == nil
+    then
+      -- '<eof>' at the end means the command is incomplete
+      if (string.find(err, "'<eof>'$"))
+        then return process (prompt2, cmd)
+        else say_line (err) --> say error mesage
+      end
+    else --> complete function
       local ret = {pcall(func)}
-      success = table.remove(ret, 1)
-      if not success -- error calling the function?
-        then print(ret[1])
-        else if ret[1]~=nil then print(unpack(ret)) end
+      local success = table.remove(ret, 1)
+      if not success --> error calling the function?
+        then say_line (ret[1]) --> error msg
+        else if ret[1] ~= nil then say_line (unpack (ret)) end
       end
   end
-until false
+end
+
+avt.initialize {title="Lua-AKFAvatar", encoding="UTF-8", audio=true}
+avt.move_in ()
+avt.say(_VERSION, " / AKFAvatar ", avt.version (), "\n\n")
+
+while true do process (prompt1, "") end
+
