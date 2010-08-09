26cc7017c675a733177313afafec6825889d4dac
Andreas K. Foerster <info@akfoerster.de>
2010-08-09 22:22:08 +0200

    lua/lua-akfavatar.lua: fix: make it save to call it directly

--- lua/lua-akfavatar.lua
+++ lua/lua-akfavatar.lua
@@ -1,5 +1,7 @@
 -- wrapper module to start a script with lua-akfavatar
 -- if the script has: require "lua-akfavatar"
 
-os.exit(os.execute("lua-akfavatar "..table.concat(arg, " ", 0)))
+if not string.find(arg[0], "lua%-akfavatar%.lua$") then
+  os.exit(os.execute("lua-akfavatar "..table.concat(arg, " ", 0)))
+end
 
