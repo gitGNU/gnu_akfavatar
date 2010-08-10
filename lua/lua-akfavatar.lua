15a2b0ae73ee882b6aaeb9df2576baf03a9282f3
Andreas K. Foerster <info@akfoerster.de>
2010-08-10 11:15:57 +0200

    lua/lua-akfavatar.lua: better way to avoid recursive loading

--- lua/lua-akfavatar.lua
+++ lua/lua-akfavatar.lua
@@ -1,7 +1,7 @@
 -- wrapper module to start a script with lua-akfavatar
 -- if the script has: require "lua-akfavatar"
 
-if not string.find(arg[0], "lua%-akfavatar%.lua$") then
+if not package.preload["lua-akfavatar"] then
   os.exit(os.execute("lua-akfavatar "..table.concat(arg, " ", 0)))
 end
 
