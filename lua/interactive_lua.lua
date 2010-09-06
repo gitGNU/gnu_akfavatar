3a6d3c6894e76f9c46199219325b72d6cb40db75
Andreas K. Foerster <info@akfoerster.de>
2010-09-06 17:43:31 +0200

    lua/interactive_lua.lua: error messages in red...

--- lua/interactive_lua.lua
+++ lua/interactive_lua.lua
@@ -9,9 +9,21 @@ print = avt.print --> define the print command
 say = print --> an alias that fits better
 
 local function interactive (cmd)
-  local function show (success, ...)
-    if select ("#", ...) > 0 then avt.print (...) end
+
+  local function error_message (...)
+    avt.set_text_color ("dark red")
+    avt.print (...)
+    avt.normal_text ()
   end
+
+  local function show (success, ...)
+    if select ("#", ...) > 0 then
+      if success
+        then avt.print (...)
+        else error_message (...)
+      end --> if success
+    end --> if select
+  end --> function show
 
   if avt.where_x () > 1 then avt.newline () end
 
@@ -24,15 +36,13 @@ local function interactive (cmd)
 
   local func, err = loadstring (cmd)
 
-  if func == nil
-    then
+  if func then show (pcall(func))
+    else --> error
       -- '<eof>' at the end means the command is incomplete
       if (string.find(err, "'<eof>'$"))
         then return interactive (cmd)
-        else avt.print (err) --> say error mesage
+        else error_message (err)
       end
-    else --> complete function
-      show (pcall(func))
   end
 
   return cmd
@@ -40,6 +50,8 @@ end
 
 avt.initialize {title="Lua-AKFAvatar", encoding="UTF-8", audio=true}
 avt.move_in ()
+avt.underlined(true)
 avt.say(_VERSION, " / AKFAvatar ", avt.version (), "\n\n")
+avt.normal_text ()
 
 while true do interactive () end
