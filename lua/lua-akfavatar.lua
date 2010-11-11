d3a706b602cdedca86db3f1b2d362b4c50e49fc8
Andreas K. Foerster <info@akfoerster.de>
2010-11-11 08:58:44 +0100

    remove lua-akfavatar.lua, install lua-akfavatar.so

--- lua/lua-akfavatar.lua
+++
@@ -1,7 +0,0 @@
--- wrapper module to start a script with lua-akfavatar
--- if the script has: require "lua-akfavatar"
-
-if not package.preload["lua-akfavatar"] then
-  os.exit(os.execute("lua-akfavatar "..table.concat(arg, " ", 0)))
-end
-
