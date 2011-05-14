f38316343ca48547ae9d4d7b50795b1af3fd6495
Andreas K. Foerster <info@akfoerster.de>
2011-05-14 13:39:52 +0200

    lua/characters_tables.lua: update Copyright notice

--- lua/characters_tables.lua
+++ lua/characters_tables.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua-akfavatar
 
--- Copyright (c) 2010 Andreas K. Foerster <info@akfoerster.de>
+-- Copyright (c) 2010,2011 Andreas K. Foerster <info@akfoerster.de>
 -- License: GPL version 3 or later
 
 -- this gives an impression over which characters are available
