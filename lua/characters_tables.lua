18a3bdcee746da5d1b66845fcf6fea6c44acfb8b
Andreas K. Foerster <info@akfoerster.de>
2011-05-14 12:16:26 +0200

    lua/characters_tables.lua: use new avt.menu()

--- lua/characters_tables.lua
+++ lua/characters_tables.lua
@@ -11,58 +11,6 @@ require "akfavatar.utf8"
 
 avt.initialize{}
 
-local items = {
-  "End",
-  "Basic Latin (U+0000-U+007F)",
-  "Latin-1 Supplement (U+0080-U+00FF)",
-  "Latin Extended-A (U+0100-U+017F)",
-  "Latin Extended-B (U+0180-U+024F)",
-  "IPA Extensions (U+0250-U+02AF)",
-  "Spacing Modifier Letters (U+02B0-U+02FF)",
-  "Combining Diacritical Marks (U+0300-U+036F)",
-  "Greek and Coptic (U+0370-U+03FF)",
-  "Cyrillic (U+0400-U+04FF)",
-  "Cyrillic Supplementary (U+0500-U+052F)",
-  "Armenian (U+0530-U+058F)",
-  "Hebrew (U+0590-U+05FF)",
-  "Thai (U+0E00-U+0E7F)",
-  "Georgian (U+10A0-U+10FF)",
-  "Ethiopic (U+1200-U+137F)",
-  "Cherokee (U+13A0-U+13FF)",
-  "Unified Canadian Aboriginal Syllabics (U+1400-U+167F)",
-  "Runic (U+16A0-U+16FF)",
-  "Latin Extended Additional (U+1E00-U+1EFF)",
-  "Greek Extended (U+1F00-U+1FFF)",
-  "General Punctuation (U+2000-U+206F)",
-  "Superscripts and Subscripts (U+2070-U+209F)",
-  "Currency Symbols (U+20A0-U+20CF)",
-  "Combining Diacritical Marks for Symbols (U+20D0-U+20FF)",
-  "Letterlike Symbols (U+2100-U+214F)",
-  "Number Forms (U+2150-U+218F)",
-  "Arrows (U+2190-U+21FF)",
-  "Mathematical Operators (U+2200-U+22FF)",
-  "Miscellaneous Technical (U+2300-U+23FF)",
-  "Control Pictures (U+2400-U+243F)",
-  "Optical Character Recognition (U+2440-U+245F)",
-  "Enclosed Alphanumerics (U+2460-U+24FF)",
-  "Box Drawing (U+2500-U+257F)",
-  "Block Elements (U+2580-U+259F)",
-  "Geometric Shapes (U+25A0-U+25FF)",
-  "Miscellaneous Symbols (U+2600-U+26FF)",
-  "Dingbats (U+2700-U+27BF)",
-  "Miscellaneous Mathematical Symbols-A (U+27C0-U+27EF)",
-  "Supplemental Arrows-A (U+27F0-U+27FF)",
-  "Braille Patterns (U+2800-U+28FF)",
-  "Supplemental Mathematical Operators (U+2A00-U+2AFF)",
-  "Free block (U+2C00-U+2E7F)",
-  "CJK Symbols and Punctuation (U+3000-U+303F)",
-  "Alphabetic Presentation Forms (U+FB00-U+FB4F)",
-  "Combining Half Marks (U+FE20-U+FE2F)",
-  "Halfwidth and Fullwidth Forms (U+FF00-U+FFEF)",
-  "Specials (U+FFF0-U+FFFF)"
-}
-
-
 local function block_list(f, t)
   local list = "╔═══╤════════╤══════════╤══════════════╤══════════════╗\n" ..
                "║ c │ number │ XML/HTML │  UTF-8 (C)   │ UTF-8 (Lua)  ║\n" ..
@@ -97,29 +45,65 @@ local function block_list(f, t)
   avt.pager(list)
 end
 
-local function show_item(item)
-  local f, t
-
-  -- get the range from the item string
-  f, t = string.match(item, "%(U%+(%x+)%-U%+(%x+)%)$")
-  f, t = tonumber(f, 16), tonumber(t, 16)
-
-  avt.show_avatar()
-  block_list(f, t)
-end
-
-
-local menu_item
-
 repeat
   avt.set_text_delay(0)
   avt.set_balloon_size()
   avt.clear()
-  menu_item = avt.long_menu (items)
 
-  if menu_item ~= 1 then
-    show_item(items[menu_item])
+  local v1, v2 = avt.menu {
+    {"End", "end"},
+    {"Basic Latin (U+0000-U+007F)", 0x0000, 0x007F},
+    {"Latin-1 Supplement (U+0080-U+00FF)", 0x0080, 0x00FF},
+    {"Latin Extended-A (U+0100-U+017F)", 0x0100, 0x017F},
+    {"Latin Extended-B (U+0180-U+024F)", 0x0180, 0x024F},
+    {"IPA Extensions (U+0250-U+02AF)", 0x0250, 0x02AF},
+    {"Spacing Modifier Letters (U+02B0-U+02FF)", 0x02B0, 0x02FF},
+    {"Combining Diacritical Marks (U+0300-U+036F)", 0x0300, 0x036F},
+    {"Greek and Coptic (U+0370-U+03FF)", 0x0370, 0x03FF},
+    {"Cyrillic (U+0400-U+04FF)", 0x0400, 0x04FF},
+    {"Cyrillic Supplementary (U+0500-U+052F)", 0x0500, 0x052F},
+    {"Armenian (U+0530-U+058F)", 0x0530, 0x058F},
+    {"Hebrew (U+0590-U+05FF)", 0x0590, 0x05FF},
+    {"Thai (U+0E00-U+0E7F)", 0x0E00, 0x0E7F},
+    {"Georgian (U+10A0-U+10FF)", 0x10A0, 0x10FF},
+    {"Ethiopic (U+1200-U+137F)", 0x1200, 0x137F},
+    {"Cherokee (U+13A0-U+13FF)", 0x13A0, 0x13FF},
+    {"Unified Canadian Aboriginal Syllabics (U+1400-U+167F)", 0x1400, 0x167F},
+    {"Runic (U+16A0-U+16FF)", 0x16A0, 0x16FF},
+    {"Latin Extended Additional (U+1E00-U+1EFF)", 0x1E00, 0x1EFF},
+    {"Greek Extended (U+1F00-U+1FFF)", 0x1F00, 0x1FFF},
+    {"General Punctuation (U+2000-U+206F)", 0x2000, 0x206F},
+    {"Superscripts and Subscripts (U+2070-U+209F)", 0x2070, 0x209F},
+    {"Currency Symbols (U+20A0-U+20CF)", 0x20A0, 0x20CF},
+    {"Combining Diacritical Marks for Symbols (U+20D0-U+20FF)", 0x20D0, 0x20FF},
+    {"Letterlike Symbols (U+2100-U+214F)", 0x2100, 0x214F},
+    {"Number Forms (U+2150-U+218F)", 0x2150, 0x218F},
+    {"Arrows (U+2190-U+21FF)", 0x2190, 0x21FF},
+    {"Mathematical Operators (U+2200-U+22FF)", 0x2200, 0x22FF},
+    {"Miscellaneous Technical (U+2300-U+23FF)", 0x2300, 0x23FF},
+    {"Control Pictures (U+2400-U+243F)", 0x2400, 0x243F},
+    {"Optical Character Recognition (U+2440-U+245F)", 0x2440, 0x245F},
+    {"Enclosed Alphanumerics (U+2460-U+24FF)", 0x2460, 0x24FF},
+    {"Box Drawing (U+2500-U+257F)", 0x2500, 0x257F},
+    {"Block Elements (U+2580-U+259F)", 0x2580, 0x259F},
+    {"Geometric Shapes (U+25A0-U+25FF)", 0x25A0, 0x25FF},
+    {"Miscellaneous Symbols (U+2600-U+26FF)", 0x2600, 0x26FF},
+    {"Dingbats (U+2700-U+27BF)", 0x2700, 0x27BF},
+    {"Miscellaneous Mathematical Symbols-A (U+27C0-U+27EF)", 0x27C0, 0x27EF},
+    {"Supplemental Arrows-A (U+27F0-U+27FF)", 0x27F0, 0x27FF},
+    {"Braille Patterns (U+2800-U+28FF)", 0x2800, 0x28FF},
+    {"Supplemental Mathematical Operators (U+2A00-U+2AFF)", 0x2A00, 0x2AFF},
+    {"Free block (U+2C00-U+2E7F)", 0x2C00, 0x2E7F},
+    {"CJK Symbols and Punctuation (U+3000-U+303F)", 0x3000, 0x303F},
+    {"Alphabetic Presentation Forms (U+FB00-U+FB4F)", 0xFB00, 0xFB4F},
+    {"Combining Half Marks (U+FE20-U+FE2F)", 0xFE20, 0xFE2F},
+    {"Halfwidth and Fullwidth Forms (U+FF00-U+FFEF)", 0xFF00, 0xFFEF},
+    {"Specials (U+FFF0-U+FFFF)", 0xFFF0, 0xFFFF}
+  }
+
+  if v1 ~= "end" then
+    block_list(v1, v2)
   end
 
-until menu_item == 1
+until v1 == "end"
 
