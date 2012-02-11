afa10cb277ad0af81e512f8a91f77b985d158ac4
Andreas K. Foerster <info@akfoerster.de>
2012-02-11 19:12:00 +0100

    pascal/multiply.pas: cannot be compiled without AKFAvatar anymore (simplified)

--- pascal/multiply.pas
+++ pascal/multiply.pas
@@ -1,12 +1,9 @@
 {*
  * simple multiplication
- * Copyright (c) 2007, 2008 Andreas K. Foerster <info@akfoerster.de>
+ * Copyright (c) 2007,2008,2012 Andreas K. Foerster <info@akfoerster.de>
  *
- * This example shows how to write programs, that use features
- * of the Avatar library, but can still be compiled without it.
- *
- * use "gpcavatar" or "fpcavatar" to compile it with AKFAvatar
- * or compile it directly with gpc or fpc to get a console program
+ * This example shows how to write programs, that use features of the 
+ * AKFAvatar library.
  *
  * This file is part of AKFAvatar
  *
@@ -31,10 +28,8 @@
 
 
 program multiply;
-
-{ use CRT if it is not compiled for AKFAvatar: }
 {$IfNDef AKFAVATAR}
-  uses CRT;
+  uses AKFAvatar;
 {$EndIf}
 
 { include image of the teacher (made with xpm2pas) }
@@ -54,9 +49,8 @@ const
   const question = 'What to exercise?';
   const q_multiplication = 'multication';
   const q_division = 'division';
-  const
-    correct = 'correct';
-    wrong = 'wrong';
+  const correct = 'correct';
+  const wrong = 'wrong';
   const q_continue = 'Do you want to continue?';
 {$EndIf}
 
@@ -64,18 +58,12 @@ const
   const question = 'Was '#252'ben?';
   const q_multiplication = 'multiplizieren';
   const q_division = 'dividieren';
-  const
-    correct = 'richtig';
-    wrong = 'falsch';
+  const correct = 'richtig';
+  const wrong = 'falsch';
   const q_continue = 'Willst du weitermachen?';
 {$EndIf}
 
-{$IfDef AKFAVATAR}
-  const multiplicationSign = chr($B7); { middle dot }
-{$Else} 
-  const multiplicationSign = '*';
-{$EndIf}
-
+const multiplicationSign = chr($B7); { middle dot }
 const divisionSign = ':';
 
 var endRequest: boolean;
@@ -104,8 +92,6 @@ if code=0
   else askResult := -1 { error }
 end;
 
-{$IfDef AKFAVATAR}
-
 procedure AskWhatToExercise;
 begin
 BalloonSize (3, 40);
@@ -122,46 +108,16 @@ case Choice(2, 2, '1', false, false) of
 ClrScr
 end;
 
-{$Else}
-
-procedure AskWhatToExercise;
-var c: char;
-begin
-ClrScr;
-WriteLn(question);
-WriteLn('1) '+ q_multiplication);
-WriteLn('2) '+ q_division);
-
-repeat
-  c := ReadKey
-until (c='1') or (c='2');
-
-case c of
-  '1' : exercise := multiplication;
-  '2' : exercise := division;
-  end;
-
-ClrScr
-end;
-
-{$EndIf}
-
 procedure sayCorrect;
 begin 
-{$IfDef AKFAVATAR} 
-  PlaySound(correctsnd, false);
-{$EndIf}
-
+PlaySound(correctsnd, false);
 TextColor(green);
 WriteLn(correct) 
 end;
 
 procedure sayWrong;
 begin 
-{$IfDef AKFAVATAR}
-  PlaySound(wrongsnd, false);
-{$EndIf}
-
+PlaySound(wrongsnd, false);
 TextColor(red); 
 WriteLn(wrong) 
 end;
@@ -171,10 +127,7 @@ var a, b, r, e: integer;
 var counter: integer;
 var isCorrect: boolean;
 begin
-{$IfDef AKFAVATAR}
-  BalloonSize (4, 40);
-{$EndIf}
-
+BalloonSize (4, 40);
 counter := 0;
 
 repeat
@@ -185,7 +138,7 @@ repeat
 
   repeat
     Write(counter:3, ') ');
-    
+
     case exercise of
     multiplication: 
       begin
@@ -213,33 +166,24 @@ end;
 
 function WantToContinue: boolean;
 begin
-{$IfDef AKFAVATAR}
-  BalloonSize (1, 40);
-  ClrScr;
-  Write(q_continue);
-
-  WantToContinue := Decide;
-{$else}
-  WantToContinue := false;
-{$EndIf}
+BalloonSize (1, 40);
+ClrScr;
+Write(q_continue);
+WantToContinue := Decide;
 end;
 
 Begin { main program }
+SetEncoding('ISO-8859-1');
+SetBackgroundColorName('tan');
 
-{ use IfDef for AKFAVATAR specific commands: }
-{$IfDef AKFAVATAR}
-  SetEncoding('ISO-8859-1');
-  SetBackgroundColorName('tan');
+{ load the image of the teacher }
+AvatarImageXPM(Addr(teacher));
 
-  { load the image of the teacher }
-  AvatarImageXPM(Addr(teacher));
+{ or use this to load the image from a file: }
+{ AvatarImageFile('teacher.xpm'); }
 
-  { or use this to load the image from a file: }
-  { AvatarImageFile('teacher.xpm'); }
-
-  correctsnd := LoadSoundData(Addr(snd_positive), sizeof(snd_positive));
-  wrongsnd   := LoadSoundData(Addr(snd_negative), sizeof(snd_negative));
-{$EndIf}
+correctsnd := LoadSoundData(Addr(snd_positive), sizeof(snd_positive));
+wrongsnd   := LoadSoundData(Addr(snd_negative), sizeof(snd_negative));
 
 Randomize;
 
@@ -249,11 +193,9 @@ repeat
   query;
 until not WantToContinue;
 
-{$IfDef AKFAVATAR}
-  FreeSound(correctsnd);
-  FreeSound(wrongsnd);
+FreeSound(correctsnd);
+FreeSound(wrongsnd);
 
-  { Avoid waiting for a keypress }
-  MoveAvatarOut;
-{$EndIf}
+{ Avoid waiting for a keypress }
+MoveAvatarOut;
 end.
