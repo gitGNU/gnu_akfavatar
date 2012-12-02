8828f40b951e85b2d0aeb9569ccd2428ceaaeaa8
Andreas K. Foerster <info@akfoerster.de>
2012-12-02 20:53:15 +0100

    lua/greeting.lua: new file

---
+++ lua/greeting.lua
@@ -0,0 +1,52 @@
+#!/usr/bin/env lua-akfavatar
+
+-- This file is dedicated to the Public Domain (CC0)
+-- http://creativecommons.org/publicdomain/zero/1.0/
+
+local avt = require "lua-akfavatar"
+
+-- translations for expressions
+avt.translations = {
+  ["Greeting"] = { de="Grüßen" },
+  ["Hello %s!"] = { de="Hallo %s!" },
+  ["you"] = { de="du da" }
+  }
+
+local L = avt.translate
+
+-- try to get realname for a username - nil on error
+-- works on POSIX compatible systems, if the name is set
+local function get_realname(username)
+  if not username then return nil end
+
+  local f = io.open ("/etc/passwd", "r")
+  if not f then return nil end
+
+  local pattern = "^"..username..":[^:]*:%d*:%d*:([^,:]+)"
+  local name
+
+  for l in f:lines() do
+    name = string.match(l, pattern)
+    if name then break end
+  end
+
+  f:close()
+
+  return name
+end
+
+-- the username can be in different environment variables
+-- depending on the system
+local username =
+  os.getenv "LOGNAME" or os.getenv "USER" or os.getenv "USERNAME"
+
+local realname = get_realname(username)
+
+avt.title(L"Greeting")
+avt.start()
+avt.avatar_image_file(avt.search "female_user.xpm")
+
+-- both realname and username may still be nil!
+avt.tell(string.format(L"Hello %s!", realname or username or L"you"))
+
+avt.wait_button()
