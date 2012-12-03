d47a76ce90f0bc8f074569535f728bb59ae0514d
Andreas K. Foerster <info@akfoerster.de>
2012-12-03 22:10:42 +0100

    avtpalette.c: add license plate

--- avtpalette.c
+++ avtpalette.c
@@ -1,3 +1,25 @@
+/*
+ * access to a palette of color definitions
+ * Copyright (c) 2012 Andreas K. Foerster <info@akfoerster.de>
+ *
+ * required standards: C99, POSIX.1-2001
+ *
+ * This file is part of AKFAvatar
+ *
+ * AKFAvatar is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AKFAvatar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "akfavatar.h"
 #include "avtinternals.h"
 #include "rgb.h"
