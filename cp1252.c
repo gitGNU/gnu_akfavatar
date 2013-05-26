1de7988b8d6191bc95daece113231930bfc97300
Andreas K. Foerster <info@akfoerster.de>
2013-05-26 11:29:41 +0200

    cp1252.c: make it more like the others

--- cp1252.c
+++ cp1252.c
@@ -1,46 +1,46 @@
 #include "avtaddons.h"
 
-static const struct avt_char_map cp1252 = {
+static const struct avt_char_map map = {
   .start = 128,.end = 159,
   .table = {
-	    L'\u20AC',		// Euro
+	    0x20AC,		// Euro
 	    INVALID_WCHAR,
-	    L'\u201A',		// single low 9 quotation mark
-	    L'\u0192',		// function symbol
-	    L'\u201E',		// double low 9 quotation mark
-	    L'\u2026',		// ellipsis
-	    L'\u2020',		// dagger
-	    L'\u2021',		// double dagger
-	    L'\u02C6',		// circumflex
-	    L'\u2030',		// promille
-	    L'\u0160',		// S caron
-	    L'\u2039',		// single left-pointing angle quotation mark
-	    L'\u0152',		// Oe ligature
+	    0x201A,		// single low 9 quotation mark
+	    0x0192,		// function symbol
+	    0x201E,		// double low 9 quotation mark
+	    0x2026,		// ellipsis
+	    0x2020,		// dagger
+	    0x2021,		// double dagger
+	    0x02C6,		// circumflex
+	    0x2030,		// promille
+	    0x0160,		// S caron
+	    0x2039,		// single left-pointing angle quotation mark
+	    0x0152,		// Oe ligature
 	    INVALID_WCHAR,
-	    L'\u017D',		// Z caron
+	    0x017D,		// Z caron
 	    INVALID_WCHAR,
 	    INVALID_WCHAR,
-	    L'\u2018',		// left single quotation mark
-	    L'\u2019',		// right single quotation mark
-	    L'\u201C',		// left double quotation mark
-	    L'\u201D',		// right double quotation mark
-	    L'\u2022',		// bullet
-	    L'\u2013',		// en dash
-	    L'\u2014',		// em dash
-	    L'\u02DC',		// small tilde
-	    L'\u2122',		// trade mark
-	    L'\u0161',		// s caron
-	    L'\u203A',		// singe right-pointing angle quotation mark
-	    L'\u0153',		// oe ligature
+	    0x2018,		// left single quotation mark
+	    0x2019,		// right single quotation mark
+	    0x201C,		// left double quotation mark
+	    0x201D,		// right double quotation mark
+	    0x2022,		// bullet
+	    0x2013,		// en dash
+	    0x2014,		// em dash
+	    0x02DC,		// small tilde
+	    0x2122,		// trade mark
+	    0x0161,		// s caron
+	    0x203A,		// singe right-pointing angle quotation mark
+	    0x0153,		// oe ligature
 	    INVALID_WCHAR,
-	    L'\u017E',		// z caron (?)
-	    L'\u0178'		// Y diaresis
+	    0x017E,		// z caron (?)
+	    0x0178		// Y diaresis
 	    }
 };
 
 
 static const struct avt_charenc converter = {
-  .data = (void *) &cp1252,
+  .data = (void *) &map,
   .to_unicode = map_to_unicode,
   .from_unicode = map_from_unicode
 };
