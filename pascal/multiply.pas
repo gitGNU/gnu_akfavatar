89799f121a2e556ad1b7c82f2c794d7e78043d48
Andreas K. Foerster <info@akfoerster.de>
2013-06-19 15:51:37 +0200

    pascal/multiply.pas: extended

--- pascal/multiply.pas
+++ pascal/multiply.pas
@@ -1,6 +1,6 @@
 {*
  * simple multiplication
- * Copyright (c) 2007,2008,2012 Andreas K. Foerster <info@akfoerster.de>
+ * Copyright (c) 2007,2008,2012,2013 Andreas K. Foerster <info@akfoerster.de>
  *
  * This example shows how to write programs, that use features of the 
  * AKFAvatar library.
@@ -45,10 +45,14 @@ const
   b_minimum = 1;
   b_maximum = 10;
 
+const NormalColor = White;
+
 {$IfDef english}
   const question = 'What to exercise?';
   const q_multiplication = 'multication';
+  const q_multiples_of = 'multiples of';
   const q_division = 'division';
+  const q_division_by = 'division by';
   const correct = 'correct';
   const wrong = 'wrong';
   const q_continue = 'Do you want to continue?';
@@ -57,7 +61,9 @@ const
 {$IfDef deutsch}
   const question = 'Was '#252'ben?';
   const q_multiplication = 'multiplizieren';
+  const q_multiples_of = 'Vielfache von';
   const q_division = 'dividieren';
+  const q_division_by = 'dividieren durch';
   const correct = 'richtig';
   const wrong = 'falsch';
   const q_continue = 'Willst du weitermachen?';
@@ -66,8 +72,11 @@ const
 const multiplicationSign = chr($B7); { middle dot }
 const divisionSign = ':';
 
+type Texercise = (multiplication, division);
+
 var endRequest: boolean;
-var exercise: (multiplication, division);
+var exercise: Texercise;
+var SpecificTable: Integer;
 
 var 
   correctsnd: Pointer = NIL;
@@ -78,7 +87,7 @@ begin
 GetRandomNumber := Random(maximum-minimum+1) + minimum
 end;
 
-function askResult: integer;
+function askNumber: integer;
 var line: string[255];
 var result, code: integer;
 begin
@@ -86,55 +95,84 @@ ReadLn(line);
 
 if line = '' then endRequest := true;
 
-val (line, result, code);
+val(line, result, code);
 if code=0
-  then askResult := result
-  else askResult := -1 { error }
+  then askNumber := result
+  else askNumber := -1 { error }
+end;
+
+procedure AskWhichTable(ex: Texercise);
+begin
+exercise := ex;
+
+repeat
+  ClrScr;
+  if exercise = multiplication
+    then Write(q_multiples_of, ' ')
+    else Write(q_division_by, ' ');
+
+  SpecificTable := askNumber;
+until SpecificTable > 0
 end;
 
 procedure AskWhatToExercise;
 begin
-BalloonSize (3, 40);
-
 ClrScr;
+setTextDelay(0);
+SpecificTable := 0;
+
 WriteLn(question);
-WriteLn('1) '+ q_multiplication);
-Write  ('2) '+ q_division);
-case Choice(2, 2, '1', false, false) of
+WriteLn;
+WriteLn('1) ', q_multiplication);
+WriteLn('2) ', q_multiples_of, ' ...');
+WriteLn('3) '+ q_division);
+Write  ('4) '+ q_division_by, ' ...');
+
+case Choice(3, 4, '1', false, false) of
   1: exercise := multiplication;
-  2: exercise := division
+  2: AskWhichTable(multiplication);
+  3: exercise := division;
+  4: AskWhichTable(division)
   end;
 
+setTextDelay(DefaultTextDelay);
 ClrScr
 end;
 
 procedure sayCorrect;
 begin 
 PlaySound(correctsnd, AVT_PLAY);
-TextColor(green);
-WriteLn(correct) 
+TextColor(LightGreen);
+WriteLn(correct);
+TextColor(NormalColor)
 end;
 
 procedure sayWrong;
 begin 
 PlaySound(wrongsnd, AVT_PLAY);
-TextColor(red); 
-WriteLn(wrong) 
+TextColor(LightRed);
+WriteLn(wrong);
+TextColor(NormalColor)
 end;
 
 procedure query;
 var a, b, r, e: integer;
-var counter: integer;
+var counter, tries: integer;
 var isCorrect: boolean;
 begin
-BalloonSize (4, 40);
 counter := 0;
 
 repeat
   inc(counter);
-  a := GetRandomNumber(a_minimum, a_maximum);
+  if SpecificTable <= 0 then
+    a := GetRandomNumber(a_minimum, a_maximum)
+  else
+    a := SpecificTable;
+
   b := GetRandomNumber(b_minimum, b_maximum);
   r := a * b;
+
+  tries := 0;
 
   repeat
     Write(counter:3, ') ');
@@ -143,22 +181,41 @@ repeat
     multiplication: 
       begin
       Write(a, multiplicationSign, b, '=');
-      e := askResult;
+      e := askNumber;
       isCorrect := e = r
       end;
     division : 
       begin
       Write(r, divisionSign, a, '=');
-      e := askResult;
+      e := askNumber;
       isCorrect := e = b
       end
     end;
     
     if not endRequest and (e<>-1) then
       begin
+      inc(tries);
       GotoXY(30, WhereY-1);
-      if isCorrect then sayCorrect else sayWrong;
-      NormVideo
+      if isCorrect then sayCorrect
+        else begin
+             sayWrong;
+
+             if tries >= 2 then { help him }
+               begin
+               HighVideo;
+               TextBackground(LightRed);
+               TextColor(Black);
+               Write(counter:3, ') ');
+               if exercise = multiplication then
+                 Write(a, multiplicationSign, b, '=', r, '  ')
+               else
+                 Write(r, divisionSign, a, '=', b, '  ');
+               NormVideo;
+               TextColor(NormalColor);
+               WriteLn;
+               isCorrect := true; { the teacher is always rght ;-) }
+               end
+             end
       end
   until isCorrect or endRequest
 until endRequest
@@ -166,7 +223,6 @@ end;
 
 function WantToContinue: boolean;
 begin
-BalloonSize (1, 40);
 ClrScr;
 Write(q_continue);
 WantToContinue := Decide;
@@ -186,6 +242,13 @@ correctsnd := LoadSoundData(Addr(snd_positive), sizeof(snd_positive), AVT_LOAD);
 wrongsnd   := LoadSoundData(Addr(snd_negative), sizeof(snd_negative), AVT_LOAD);
 
 Randomize;
+
+setAvatarMode(footer);
+MoveAvatarIn;
+
+setBalloonColor(0, $55, 0);
+TextColor(NormalColor);
+Window(20, 1, 20+40, ScreenSize.y);
 
 repeat
   endRequest := false;
